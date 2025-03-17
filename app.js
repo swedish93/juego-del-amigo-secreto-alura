1e22dd932300da8b285384610a84d23792fc8feb
swedish93 <danielcclswedish93@gmail.com>
2025-03-17 14:08:10 -0600

    Se actualiza el challenge

--- app.js
+++ app.js
@@ -23,3 +23,23 @@ function actualizarListaHTML() {
     });
 }
 
+function sortearAmigo (){
+    if (listaAmigos.length === 0) {
+        alert("La lista está vacía, agrega amigos primero.");
+        return;
+    }
+    let amigoAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSeleccionado = listaAmigos[amigoAleatorio];
+
+    mostrarAmigoSorteado(amigoSeleccionado);
+    }
+
+function mostrarAmigoSorteado(amigoSeleccionado){
+    let ul2 = document.getElementById('resultado');
+    let li = document.createElement("li");
+    li.textContent = "El amigo seleccionado es " + amigoSeleccionado;
+    ul2.appendChild(li);
+    let ul = document.getElementById('listaAmigosUl');
+    ul.innerHTML = "";
+
+}
