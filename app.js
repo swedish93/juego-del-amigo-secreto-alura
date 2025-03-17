a349018725bc642222a46331aaa90c3a7d8fc311
swedish93 <danielcclswedish93@gmail.com>
2025-03-17 11:18:07 -0600

    Se actualiza el challenge

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function añadirListaAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+            if (nombreAmigo === "") {
+            alert ('Ingrese nombre válido')
+        } else {
+            listaAmigos.push(nombreAmigo)
+            document.getElementById('amigo').value= "";
+        }
+
+    console.log (listaAmigos);
+}
